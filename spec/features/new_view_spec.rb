ba094321acf84a881f140db295696931f30c44ff
Vinícius Ferreira <viniciustferreira@gmail.com>
2017-10-24 18:07:47 -0200

    Inserting a new multi_task to support the second test.

--- spec/features/new_view_spec.rb
+++ spec/features/new_view_spec.rb
@@ -5,6 +5,20 @@ require 'date'
 include Common
 
 RSpec.describe 'creation of a new task' do
+  before(:all) do
+    if !MultiTask.where(description: "testing2").exists?
+      task = MultiTask.new(
+        {
+          start_date: Time.now,
+          end_date:   Time.now + 10*60,
+          description: "testing2",
+          priority: 10,
+          frequency: 1 
+        }
+      )
+      task.save
+    end
+  end
 
   it 'creates a new task' do
     date = Date.new(2017,01,01)
@@ -18,8 +32,8 @@ RSpec.describe 'creation of a new task' do
 
     click_button 'Create'
     expect(page).to have_content("Tarefa criada com sucesso.")
-
   end
+
   it 'creates a new subtask' do
     date = Date.new(2017,01,01)
     visit '/multi_tasks/new'
