d57841e2a67b7869b3d7b16b6c9751bca063f823
Vinícius Ferreira <viniciustferreira@gmail.com>
2018-02-27 22:05:47 -0300

    Applying option buttons

--- app/helpers/multi_tasks_helper.rb
+++ app/helpers/multi_tasks_helper.rb
@@ -1,2 +1,16 @@
 module MultiTasksHelper
+
+  def options(task)
+    options = [ link_to('Detalhes', task, class: "dropdown-item"),
+     link_to('Editar', edit_multi_task_path(task), class: "dropdown-item"),
+     link_to('Excluir', task, method: :delete, data: { confirm: 'Excluir: Tem certeza?' }, class: "dropdown-item") ]
+
+    content_tag :div, class: "dropdown " do
+      button =  button_tag 'Opções', class: 'btn btn-secondary dropdown-toggle', data: { toggle: :dropdown }
+      content = content_tag :div, class: "dropdown-menu" do
+        options.join.html_safe
+      end
+      (button + content).html_safe
+    end
+  end
 end
